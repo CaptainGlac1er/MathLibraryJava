320232c11a4ca78aba2307691b3ce1a68b9ba90d
George Colgrove <georgecolgove@outlook.com>
2017-04-01 19:28:01 -0400

    Added matrix multiplication.

--- src/gwc/application/Main.java
+++ src/gwc/application/Main.java
@@ -4,12 +4,37 @@ import gwc.mathlibrary.*;
 public class Main {
 
     public static void main(String[] args) {
-	    double[][] test = new double[4][4];
-	    int cnt = 1;
-	    Vector vector1 = new Vector(5,-5,3);
-	    Vector vector2 = new Vector(1,-6.5,5);
-
-	    System.out.println(vector1.crossProduct(vector2));
-
+	    testMatrixMultiplication();
+        //testVectorProduct();
     }
+    public static void testMatrixMultiplication(){
+		double[][] test = new double[1][5];
+		for(int x = 0; x < test.length; x++){
+			for(int y = 0; y < test[x].length; y++){
+				test[x][y] = (Math.random() * 10) - 5;
+			}
+		}
+		double[][] test1 = new double[5][2];
+		for(int x = 0; x < test1.length; x++){
+			for(int y = 0; y < test1[x].length; y++){
+				test1[x][y] = (Math.random() * 10) - 5;
+			}
+		}
+		Matrix matrix1 = new Matrix(test);
+		matrix1.printMatrix();
+		System.out.println("\r\n\r\n");
+
+		Matrix matrix2 = new Matrix(test1);
+		matrix2.printMatrix();
+
+		System.out.println("\r\n\r\n");
+		matrix1.multiply(matrix2).printMatrix();
+	}
+    public static void testVectorProduct(){
+		int cnt = 1;
+		Vector vector1 = new Vector(5,-5,3);
+		Vector vector2 = new Vector(1,-6.5,5);
+
+		System.out.println(vector1.crossProduct(vector2));
+	}
 }
--- src/gwc/mathlibrary/Matrix.java
+++ src/gwc/mathlibrary/Matrix.java
@@ -4,7 +4,14 @@ package gwc.mathlibrary;
  * Created by George on 3/28/2017.
  */
 public class Matrix {
-    public static double determinant(double[][] array){
+    private double[][] array;
+    public Matrix(double[][] array){
+        this.array = array;
+    }
+    public Matrix(int rows, int columns){
+        array = new double[rows][columns];
+    }
+    public double getDeterminant(){
         double output = 0;
         if(array.length == 1){
             return array[0][0];
@@ -25,5 +32,56 @@ public class Matrix {
         }
         return output;
     }
-
+    public static double determinant(double[][] array){
+        return (new Matrix(array)).getDeterminant();
+    }
+    public int getColumnCount(){
+        return array[0].length;
+    }
+    public int getRowCount(){
+        return array.length;
+    }
+    public double[] getColumnArray(int num){
+        double[] output = new double[getRowCount()];
+        for(int i = 0; i < output.length; i++)
+            output[i] = getValue(num, i);
+        return output;
+    }
+    public double[] getRowArray(int num){
+        double[] output = new double[getColumnCount()];
+        for(int i = 0; i < output.length; i++)
+            output[i] = getValue(i, num);
+        return output;
+    }
+    public double getValue(int col, int row){
+        return array[row][col];
+    }
+    public void setValue(int col, int row, double value){
+        array[row][col] = value;
+    }
+    public Matrix multiply(Matrix second){
+        int m = getColumnCount();
+        if(m != second.getRowCount())
+            return null;
+        Matrix newMatrix = new Matrix(getRowCount(), second.getColumnCount());
+        for(int y = 0; y < newMatrix.getRowCount(); y++){
+            for(int x = 0; x < newMatrix.getColumnCount(); x++){
+                double cellValue = 0;
+                double[] rowData = getRowArray(y);
+                double[] colData = second.getColumnArray(x);
+                for (int i = 0; i < rowData.length; i++)
+                    cellValue += rowData[i] * colData[i];
+                newMatrix.setValue(x, y, cellValue);
+            }
+        }
+        return newMatrix;
+    }
+    public void printMatrix(){
+        for(int y = 0; y < getRowCount(); y++){
+            for(int x = 0; x < getColumnCount(); x++){
+                System.out.print(getValue(x,y) + " ");
+            }
+            System.out.println();
+        }
+    }
 }
